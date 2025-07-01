16ceae91f440d967736abc95abb9a082828ccfee
danielpf30 <danielparnaiba1030@gmail.com>
2025-07-01 01:15:03 -0300

    implementacao do botao curtir

--- scripts.js
+++ scripts.js
@@ -18,8 +18,36 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+const contatoForm = document.getElementById('contatoForm');
+// Verifica se o formulário de contato realmente existe nesta página
+if (contatoForm) {
+    contatoForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert('Mensagem enviada com sucesso!');
+    });
+}
 
-document.getElementById('contatoForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
+document.addEventListener('DOMContentLoaded', () => {
+    const curtirBtn = document.getElementById('btCurtir');
+    const contador = document.getElementById('contador');
+
+    const receitaId = window.location.pathname; // usa o caminho da página como chave única
+    const curtidasSalvas = JSON.parse(localStorage.getItem("curtidasReceitas")) || {};
+
+    
+    if (curtidasSalvas[receitaId]) {
+        contador.textContent = curtidasSalvas[receitaId];
+    }
+
+    curtirBtn.addEventListener('click', () => {
+        const atual = parseInt(contador.textContent);
+        const novoValor = atual + 1;
+        contador.textContent = novoValor;
+
+        curtidasSalvas[receitaId] = novoValor;
+        localStorage.setItem("curtidasReceitas", JSON.stringify(curtidasSalvas));
+
+        curtirBtn.classList.add('curtido');
+    });
 });
+
